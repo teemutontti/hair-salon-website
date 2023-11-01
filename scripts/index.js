f69884fa7ba642497739991f3442ca806304e8ac
Teemu Tontti <teemu.tontti.tt@gmail.com>
2023-11-02 01:24:46 +0200

    scripts/index.js: Add eventListener for hamburger menu click.

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// Hamburger menu click event
+const menuButton = document.getElementById("menu-button");
+menuButton.addEventListener("click", () => {
+    const icon = menuButton.querySelector(".material-symbols-outlined");
+    const links = document.getElementById("links");
+    if (icon.innerHTML == "menu") {
+        icon.innerHTML = "close";
+        links.style.display = "block";
+    } else {
+        icon.innerHTML = "menu";
+        links.style.display = "none";
+    }
+});
