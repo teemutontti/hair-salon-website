46f5853856346da298219637ca2e54941ec9e747
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-09-17 21:47:40 +0300

    Add limiter for only 1 reservation per 15 min.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const dbConnection = require("./database/connection");
 
 const port = 8080;
@@ -9,12 +10,17 @@ app.use(cors());
 app.use(express.static("./frontend/dist"));
 app.use(express.json());
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1,
+    message: "You have reached you reservation limit."
+});
+
 app.get("/reservations", async (req, res) => {
     try {
         const reservations = await dbConnection.getAllReservations();
         res.status(200).json(reservations);
     } catch (err) {
-        console.error(err);
         res.status(500).json(err);
     }
 });
@@ -24,7 +30,6 @@ app.get("/schedule/default", async (req, res) => {
         const scehdule = await dbConnection.getDefaultSchedule();
         res.status(200).json(scehdule);
     } catch (err) {
-        console.error(err);
         res.status(500).json(err);
     }
 });
@@ -34,18 +39,15 @@ app.get("/schedule/exception", async (req, res) => {
         const schedule = await dbConnection.getExceptionSchedule();
         res.status(200).json(schedule);
     } catch (err) {
-        console.error(err);
         res.status(500).json(err);
     }
 });
 
-app.post("/reservations", async (req, res) => {
-    console.log("req in index: ", req.body);
+app.post("/reserve", limiter, async (req, res) => {
     try {
         const reservation = await dbConnection.saveReservation(req.body);
         res.status(201).json(reservation);
     } catch (err) {
-        console.error(err);
         res.status(500).json(err);
     }
 });
